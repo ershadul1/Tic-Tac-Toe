b72919865367a1fd961b5e193d0804dc05c3eeed
rahalrazika <rere055@gmail.com>
2020-07-28 14:19:46 +0200

    add the welcome interface

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,9 @@
 #!/usr/bin/env ruby
-puts 'Hello World!'
+puts "Welcome to Tic Tac Toe "
+puts "Input Name of Player 1"
+player_1 = get.chomp
+puts "Input Name of Player 2"
+player_2 = get.chomp
+puts "Welcome #{player_1} [X]  and #{player_2} [O]"
+#Start the game 
+#show the board 
