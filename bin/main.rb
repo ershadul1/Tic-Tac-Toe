8228e93a07f09cfd26af919262113ab3f81b7025
ershadul1 <ershadulrayhan@gmail.com>
2020-07-29 18:21:19 +0600

    Add validate method in move class

--- bin/main.rb
+++ bin/main.rb
@@ -1,11 +1,6 @@
 #!/usr/bin/env ruby
- 
-class players (name, symbol)
-  attr_reader :name
-  @name = name 
-  @symbol = symbol
-end
- 
+require_relative '../lib/methods.rb'
+
 def display_board(arr)
   puts " #{arr[0]} | #{arr[1]} | #{arr[2]}"
   puts '---|---|---'
@@ -13,32 +8,48 @@ def display_board(arr)
   puts '---|---|---'
   puts " #{arr[6]} | #{arr[7]} | #{arr[8]}"
 end
+
+arr = %w[1 2 3 4 5 6 7 8 9]
+
+arr_two = []
+
 puts 'Welcome to Tic Tac Toe'
 puts 'Input Name of the Player 1'
-player_one = players.new(gets.chomp, X)
-puts player_one = player_one.name
+player_one = gets.chomp
+puts 'Input Name of the Player 2'
+player_two = gets.chomp
+puts "Welcome #{player_one} [X]  and #{player_two} [O]"
 
+puts 'The game has started'
+puts 'This is the board'
+display_board(arr)
+# A loop will start here, and continue untill game is won/drawn
 
-# puts 'Welcome to Tic Tac Toe'
-# puts 'Input Name of the Player 1'
-# player_one = gets.chomp
-# puts 'Input Name of the Player 2'
-# player_two = gets.chomp
-# puts "Welcome #{player_one} [X]  and #{player_two} [O]"
-# puts 'The game has started'
-# puts 'This is the board'
-# # Show the board
-# # A loop will start here, and continue untill game is won/drawn
-# puts "Turn of #{player_one}:"
-# input = gets.chomp
-# puts "Your input was #{input}"
-# puts 'This is the board now'
-# # Show the board
-# puts "Turn of #{player_two}:"
-# input = gets.chomp
-# puts "Your input was #{input}"
-# puts 'This is the board now'
-# # Show the board
+game_finish = false
+i = 1
+while game_finish == false && i < 10
+  player = i.even? ? player_two : player_one
+  valid_move = false
+  while valid_move == false
+    puts "Turn of #{player}:"
+    input = gets.chomp
+    move = Move.new(input)
+    move.validate(arr, arr_two) == true ? (valid_move = true) : (puts 'Wrong input! Try again')
+  end
+  puts "Your input was #{input}"
+  arr_two << input
+  move.apply_input(arr, i)
+  puts 'This is the board now'
+  display_board(arr)
+  game_finish = win_checker(arr) ? true : false
+  i += 1
+end
+
+if i > 9
+  puts 'This game is a draw'
+else
+  win_checker(arr) == 'X' ? (puts "#{player_one} has won the game") : (puts "#{player_two} has won the game")
+end
 
 # # After finishing the game
 # # if there is a win
