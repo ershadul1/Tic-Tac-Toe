c8da3422f163fdd7edc759005fe586840d3acf9e
ershadul1 <ershadulrayhan@gmail.com>
2020-07-29 18:43:38 +0600

    Modify win checker method

--- lib/methods.rb
+++ lib/methods.rb
@@ -15,78 +15,19 @@ class Move
   def apply_input(arr, ite)
     arr[@input.to_i - 1] = ite.odd? ? 'X' : 'O'
   end
+end
 
+def equality_checker(arg1, arg2, arg3)
+  if arg1 == arg2 && arg2 == arg3
+    true
+  else
+    false
+  end
 end
 
 def win_checker(arr)
-  if arr[0] == arr[1] && arr[1] == arr[2]
-    arr[0]
-  elsif arr[3] == arr[4] && arr[4] == arr[5]
-    arr[3]
-  elsif arr[6] == arr[7] && arr[7] == arr[8]
-    arr[6]
-  elsif arr[0] == arr[3] && arr[3] == arr[6]
-    arr[0]
-  elsif arr[1] == arr[4] && arr[4] == arr[7]
-    arr[1]
-  elsif arr[2] == arr[5] && arr[5] == arr[8]
-    arr[2]
-  elsif arr[0] == arr[4] && arr[4] == arr[8]
-    arr[0]
-  elsif arr[2] == arr[4] && arr[4] == arr[6]
-    arr[2]
-  end
+  [0, 3, 6].each { |i| true if equality_checker(arr[i], arr[i + 1], arr[i + 2]) }
+  [0, 1, 2].each { |i| true if equality_checker(arr[i], arr[i + 3], arr[i + 6]) }
+  true if equality_checker(arr[0], arr[4], arr[8])
+  true if equality_checker(arr[2], arr[4], arr[6])
 end
-# class GameTest
-#   include Game
-#   move = Move.new("X")
-#   move = Move.new("O")
-#   move.add_value
-#   p move.validate
-# end
-
-# p test = GameTest.new
-
-# module Board
-#   def display_board(arr)
-#     puts " #{arr[0]} | #{arr[1]} | #{arr[2]}"
-#     puts '---|---|---'
-#     puts " #{arr[3]} | #{arr[4]} | #{arr[5]}"
-#     puts '---|---|---'
-#     puts " #{arr[6]} | #{arr[7]} | #{arr[8]}"
-#   end
-
-#   def win_checker(arr)
-#     if arr[0] == arr[1] && arr[1] == arr[2]
-#       arr[0]
-#     elsif arr[3] == arr[4] && arr[4] == arr[5]
-#       arr[3]
-#     elsif arr[6] == arr[7] && arr[7] == arr[8]
-#       arr[6]
-#     elsif arr[0] == arr[3] && arr[3] == arr[6]
-#       arr[0]
-#     elsif arr[1] == arr[4] && arr[4] == arr[7]
-#       arr[1]
-#     elsif arr[2] == arr[5] && arr[5] == arr[8]
-#       arr[2]
-#     elsif arr[0] == arr[4] && arr[4] == arr[8]
-#       arr[0]
-#     elsif arr[2] == arr[4] && arr[4] == arr[6]
-#       arr[2]
-#     else
-#       -1
-#     end
-#   end
-# end
-
-# arr = %w[O 2 X O X 6 O 8 X]
-
-# display_board(arr)
-
-# if win_checker(arr) == 'X'
-#   puts 'Winner is Player-1'
-# elsif win_checker(arr) == 'O'
-#   puts 'Winner is Player-2'
-# else
-#   puts 'Draw'
-# end
