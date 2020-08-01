b4d9cdbe523bf96abb1e8e25758ed502fcf59619
ershadul1 <ershadulrayhan@gmail.com>
2020-08-01 22:15:20 +0600

    Add blank lines in between code blocks in main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
+
 require_relative '../lib/move.rb'
 require_relative '../lib/game_result.rb'
+
 def display_board(arr)
   puts " #{arr[0]} | #{arr[1]} | #{arr[2]}"
   puts '---|---|---'
@@ -8,8 +10,10 @@ def display_board(arr)
   puts '---|---|---'
   puts " #{arr[6]} | #{arr[7]} | #{arr[8]}"
 end
+
 arr = %w[1 2 3 4 5 6 7 8 9]
 arr_two = []
+
 puts 'Welcome to Tic Tac Toe'
 puts 'Input Name of the Player 1'
 player_one = gets.chomp
@@ -18,18 +22,22 @@ player_two = gets.chomp
 puts "Welcome #{player_one} [X]  and #{player_two} [O]"
 puts 'The game has started'
 puts 'This is the board'
+
 display_board(arr)
 game_finish = false
 i = 1
+
 while game_finish == false && i < 10
   player = i.even? ? player_two : player_one
   valid_move = false
+
   while valid_move == false
     puts "Turn of #{player}:"
     input = gets.chomp
     move = Move.new(input)
     move.validate(arr, arr_two) == true ? (valid_move = true) : (puts 'Wrong input! Try again')
   end
+
   puts "Your input was #{input}"
   arr_two << input
   move.apply_input(arr, i)
@@ -39,6 +47,7 @@ while game_finish == false && i < 10
   game_finish = game_result.win_checker(arr) ? true : false
   i += 1
 end
+
 if game_finish == false
   puts 'This game is a draw'
 elsif game_result.win_checker(arr) == 'X'
