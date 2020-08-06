52b60f74365084a6931fbf05a1cc1639388ccd98
ershadul1 <ershadulrayhan@gmail.com>
2020-08-07 00:44:44 +0600

    Add game intro module

--- bin/main.rb
+++ bin/main.rb
@@ -2,6 +2,7 @@
 
 require './lib/move.rb'
 require './lib/game_result.rb'
+require './lib/game_intro.rb'
 
 def display_board(arr)
   puts ''
@@ -13,44 +14,50 @@ def display_board(arr)
   puts ''
 end
 
-arr = %w[1 2 3 4 5 6 7 8 9]
 
-puts 'Welcome to Tic Tac Toe'
-print 'Input Name of the Player 1: '
-player_one = gets.chomp
-print 'Input Name of the Player 2: '
-player_two = gets.chomp
-puts "Welcome #{player_one} [X]  and #{player_two} [O]"
-puts 'The game has started'
-puts 'This is the board'
+class Game 
+  include GameIntro
+  arr = %w[1 2 3 4 5 6 7 8 9]
+  puts WELCOME_MESSAGE
+  puts GAME_INSTRUCTIONS
+  print 'Input Name of the Player 1: '
+  player_one = gets.chomp
+  print 'Input Name of the Player 2: '
+  player_two = gets.chomp
+  puts "Welcome #{player_one} [X]  and #{player_two} [O]"
+  puts 'The game has started'
+  puts 'This is the board'
 
-display_board(arr)
+  display_board(arr)
 
-i = 1
-while i < 10
-  player = i.even? ? player_two : player_one
-  valid_move = false
+  i = 1
+  while i < 10
+    player = i.even? ? player_two : player_one
+    valid_move = false
 
-  while valid_move == false
-    puts "Turn of #{player}:"
-    input = gets.chomp
-    move = Move.new(input)
-    move.validate(arr) == true ? (valid_move = true) : (puts 'Wrong input! Try again')
+    while valid_move == false
+      puts "Turn of #{player}:"
+      input = gets.chomp
+      move = Move.new(input)
+      move.validate(arr) == true ? (valid_move = true) : (puts 'Wrong input! Try again')
+    end
+
+    puts "Your input was #{input}"
+    move.apply_input(arr, i)
+    puts 'This is the board now'
+    display_board(arr)
+    game_result = GameResult.new
+    i = 10 if game_result.win_checker(arr) 
+    i += 1
   end
 
-  puts "Your input was #{input}"
-  move.apply_input(arr, i)
-  puts 'This is the board now'
-  display_board(arr)
-  game_result = GameResult.new
-  i = 10 if game_result.win_checker(arr) 
-  i += 1
+  if game_result.win_checker(arr) == 'X'
+    puts "#{player_one} [X] has won the game"
+  elsif game_result.win_checker(arr) == 'O'
+    puts "#{player_two} [O] has won the game"
+  else
+    puts 'This game is a draw'
+  end
 end
 
-if game_result.win_checker(arr) == 'X'
-  puts "#{player_one} [X] has won the game"
-elsif game_result.win_checker(arr) == 'O'
-  puts "#{player_two} [O] has won the game"
-else
-  puts 'This game is a draw'
-end
+Game.new
---
+++ lib/game_intro.rb
@@ -0,0 +1,16 @@
+module GameIntro
+  WELCOME_MESSAGE = "
+                     * * * * * * * * * * * * * * 
+                     *  WELCOME TO TIC TAC TOE *
+                     * * * * * * * * * * * * * *"
+  GAME_INSTRUCTIONS = "
+      How to play this game?
+
+      1. Input player names and press Enter
+      2. Input your desired cell number in each step untill the game is over
+      3. In order to win the game, a player must place three of their marks
+         in a horizontal, vertical, or diagonal row.
+      4. If no player wins before filling all cells, then the game is a draw
+      "
+
+end
