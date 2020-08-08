19a995aef1aa92837484863942d1c307cb1dbbf6
ershadul1 <ershadulrayhan@gmail.com>
2020-08-08 12:15:16 +0600

    Add spaces and newline to improve game interface

--- bin/main.rb
+++ bin/main.rb
@@ -7,12 +7,6 @@ require_relative '../lib/game_intro.rb'
 class Game
   include GameIntro
 
-  def game_reset
-    @arr = %w[1 2 3 4 5 6 7 8 9]
-    @i = 1
-    @game_result = GameResult.new
-  end
-
   def game_run
     game_reset
     show_intro
@@ -21,17 +15,21 @@ class Game
     play_again
   end
 
+  def game_reset
+    @arr = %w[1 2 3 4 5 6 7 8 9]
+    @i = 1
+    @game_result = GameResult.new
+  end
+
   def show_intro
     puts WELCOME_MESSAGE
     puts GAME_INSTRUCTIONS
-    print 'Input Name of the Player 1: '
+    print ' Input Name of the Player 1: '
     @player_one = gets.chomp
-    print 'Input Name of the Player 2: '
+    print ' Input Name of the Player 2: '
     @player_two = gets.chomp
-    puts ''
-    puts "Welcome! #{@player_one} [X]  and #{@player_two} [O].  The game has started"
-    puts ''
-    puts 'This is the board'
+    puts "\n Welcome! #{@player_one} [X]  and #{@player_two} [O].  The game has started"
+    puts "\n This is the board"
     display_board(@arr)
   end
 
@@ -40,15 +38,15 @@ class Game
       valid_move = false
 
       while valid_move == false
-        puts "Turn of #{player_selector}:"
+        print "\n Turn of #{player_selector}: "
         input = gets.chomp
         move = Move.new(input)
-        move.validate(@arr) == true ? (valid_move = true) : (puts 'Wrong input! Try again')
+        move.validate(@arr) == true ? (valid_move = true) : (puts ' Wrong input! Try again ')
       end
 
-      puts "Your input was #{input}"
+      puts " Your input was #{input} "
       move.apply_input(@arr, @i)
-      puts 'This is the board now'
+      puts ' This is the board now '
       display_board(@arr)
       @i = 10 if @game_result.win_checker(@arr)
       @i += 1
@@ -57,16 +55,16 @@ class Game
 
   def game_over
     if @game_result.win_checker(@arr) == 'X'
-      puts "#{@player_one} [X] has won the game"
+      puts " #{@player_one} [X] has won the game "
     elsif @game_result.win_checker(@arr) == 'O'
-      puts "#{@player_two} [O] has won the game"
+      puts " #{@player_two} [O] has won the game "
     else
-      puts 'This game is a draw'
+      puts ' This game is a draw '
     end
   end
 
   def play_again
-    puts "Do you want to play again?(y/n)"
+    print " Do you want to play again?(y/n) : "
     game_run if gets.chomp == 'y'
   end
 
@@ -75,13 +73,11 @@ class Game
   end
 
   def display_board(arr)
-    puts ''
-    puts " #{arr[0]} | #{arr[1]} | #{arr[2]}"
-    puts '---|---|---'
-    puts " #{arr[3]} | #{arr[4]} | #{arr[5]}"
-    puts '---|---|---'
-    puts " #{arr[6]} | #{arr[7]} | #{arr[8]}"
-    puts ''
+    puts "\n     #{arr[0]} | #{arr[1]} | #{arr[2]}"
+    puts '    ---|---|---'
+    puts "     #{arr[3]} | #{arr[4]} | #{arr[5]}"
+    puts '    ---|---|---'
+    puts "     #{arr[6]} | #{arr[7]} | #{arr[8]}"
   end
 end
 
