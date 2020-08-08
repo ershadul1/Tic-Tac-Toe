49e0cbd0f9af1e88ceabc3895a7c4bb372493725
ershadul1 <ershadulrayhan@gmail.com>
2020-08-08 11:43:21 +0600

    Refactor game procedure into game_engine method

--- bin/main.rb
+++ bin/main.rb
@@ -4,25 +4,18 @@ require_relative '../lib/move.rb'
 require_relative '../lib/game_result.rb'
 require_relative '../lib/game_intro.rb'
 
-def display_board(arr)
-  puts ''
-  puts " #{arr[0]} | #{arr[1]} | #{arr[2]}"
-  puts '---|---|---'
-  puts " #{arr[3]} | #{arr[4]} | #{arr[5]}"
-  puts '---|---|---'
-  puts " #{arr[6]} | #{arr[7]} | #{arr[8]}"
-  puts ''
-end
-
 class Game
   include GameIntro
 
   def initialize
     @arr = %w[1 2 3 4 5 6 7 8 9]
+    @i = 1
   end
 
   def game_run
     show_intro
+    game_engine
+    game_over
   end
 
   def show_intro
@@ -39,35 +32,49 @@ class Game
     display_board(@arr)
   end
 
-  display_board(arr)
+  def game_engine
+    while @i < 10
+      valid_move = false
 
-  i = 1
-  while i < 10
-    player = i.even? ? player_two : player_one
-    valid_move = false
+      while valid_move == false
+        puts "Turn of #{player_selector}:"
+        input = gets.chomp
+        move = Move.new(input)
+        move.validate(@arr) == true ? (valid_move = true) : (puts 'Wrong input! Try again')
+      end
 
-    while valid_move == false
-      puts "Turn of #{player}:"
-      input = gets.chomp
-      move = Move.new(input)
-      move.validate(arr) == true ? (valid_move = true) : (puts 'Wrong input! Try again')
+      puts "Your input was #{input}"
+      move.apply_input(@arr, @i)
+      puts 'This is the board now'
+      display_board(@arr)
+      game_result = GameResult.new
+      @i = 10 if game_result.win_checker(@arr)
+      @i += 1
     end
-
-    puts "Your input was #{input}"
-    move.apply_input(arr, i)
-    puts 'This is the board now'
-    display_board(arr)
-    game_result = GameResult.new
-    i = 10 if game_result.win_checker(arr)
-    i += 1
   end
 
-  if game_result.win_checker(arr) == 'X'
-    puts "#{player_one} [X] has won the game"
-  elsif game_result.win_checker(arr) == 'O'
-    puts "#{player_two} [O] has won the game"
-  else
-    puts 'This game is a draw'
+  def game_over
+    if game_result.win_checker(@arr) == 'X'
+      puts "#{@player_one} [X] has won the game"
+    elsif game_result.win_checker(@arr) == 'O'
+      puts "#{@player_two} [O] has won the game"
+    else
+      puts 'This game is a draw'
+    end
+  end
+
+  def player_selector
+    @i.even? ? @player_two : @player_one
+  end
+
+  def display_board(arr)
+    puts ''
+    puts " #{arr[0]} | #{arr[1]} | #{arr[2]}"
+    puts '---|---|---'
+    puts " #{arr[3]} | #{arr[4]} | #{arr[5]}"
+    puts '---|---|---'
+    puts " #{arr[6]} | #{arr[7]} | #{arr[8]}"
+    puts ''
   end
 end
 
